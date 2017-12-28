f22d9ae8c02ad89e6168be5595ff30a04194b66a
VladimirBelyusenko <beluysenko96@gmail.com>
2017-12-28 18:56:33 +0300

    added Entity class .h, .cpp

---
+++ GPROject/Game/Game/Entity.h
@@ -0,0 +1,30 @@
+#ifndef _ENTITY_HEADER_H_
+#define _ENTITY_HEADER_H_
+#include <SFML\Graphics.hpp>
+#include <iostream>
+
+using namespace sf;
+using namespace std;
+
+
+class Entity {
+public:
+	enum { left, right, up, down, stay } state;// тип перечислени€ - состо€ние объекта
+	float dx, dy, x, y, speed, moveTimer;//добавили переменную таймер дл€ будущих целей
+	int w, h, health; //переменна€ УhealthФ, хран€ща€ жизни игрока
+	bool life; //переменна€ УlifeФ жизнь, логическа€
+	Texture texture;//сфмл текстура
+	Sprite sprite;//сфмл спрайт 
+	float CurrentFrame;//хранит текущий кадр
+	std::string name;//враги могут быть разные, врагов можно различать по именам
+					 //каждому можно дать свое действие в update() в зависимости от имени
+	virtual void update(float time) = 0;
+	Entity(Image &image, float X, float Y, int W, int H, std::string Name);
+	FloatRect getRect();
+	virtual ~Entity()
+	{
+		std::cout << "Parent died";
+	};
+
+};
+#endif
--- GPROject/Game/Game/Game.cpp
+++ GPROject/Game/Game/Game.cpp
@@ -2,6 +2,7 @@
 #include <iostream> 
 #include <SFML/Graphics.hpp>
 #include "map.h" //подключили код с картой
+#include "Entity.h"
 #include <sstream> //текст
 
 #include <list>
@@ -9,46 +10,7 @@
 
 using namespace sf;
 //////////////////////////// Ћј—— —”ўЌќ—“№////////////////////////
-class Entity {
-public:
-	enum { left, right, up, down, stay } state;// тип перечислени€ - состо€ние объекта
-	float dx, dy, x, y, speed, moveTimer;//добавили переменную таймер дл€ будущих целей
-	int w, h, health; //переменна€ УhealthФ, хран€ща€ жизни игрока
-	bool life; //переменна€ УlifeФ жизнь, логическа€
-	Texture texture;//сфмл текстура
-	Sprite sprite;//сфмл спрайт 
-	float CurrentFrame;//хранит текущий кадр
-	std::string name;//враги могут быть разные, врагов можно различать по именам
-					 //каждому можно дать свое действие в update() в зависимости от имени
 
-	Entity(Image &image, float X, float Y, int W, int H, std::string Name) {
-		x = X; y = Y; //координата по€влени€ спрайта
-		w = W; h = H;
-		name = Name;
-		moveTimer = 0;
-		dx = 0; dy = 0;
-		speed = 0;
-		CurrentFrame = 0;
-		health = 100;
-		life = true; //инициализировали логическую переменную жизни, герой жив
-		texture.loadFromImage(image); //заносим наше изображение в текстуру
-		sprite.setTexture(texture); //заливаем спрайт текстурой
-	}
-	virtual ~Entity ()
-	{
-		std::cout << "Parent died";
-	}
-	FloatRect getRect() {//метод получени€ пр€моугольника. его коорд, размеры (шир,высот).
-		FloatRect FR(x, y, w, h); // переменна€ FR типа FloatRect
-		return FR;
-		//return FloatRect(x, y, w, h);
-		//“ип данных (класс) "sf::FloatRect" позвол€ет хранить четыре координаты пр€моугольника
-		//в нашей игре это координаты текущего расположени€ тайла на карте
-		//далее это позволит спросить, есть ли ещЄ какой-либо тайл на этом месте 
-		//эта ф-ци€ нужна дл€ проверки пересечений 
-	}
-	virtual void update(float time) = 0;
-};
 
 //////////////////////////////////////////////////// Ћј—— »√–ќ ј////////////////////////
 class Player :public Entity {
@@ -508,7 +470,6 @@ int main()
 			float time = clock.getElapsedTime().asMicroseconds();
 
 			int immtime = immClock.getElapsedTime().asMilliseconds();
-			bool checkt;
 
 			if (p.life) gameTime = gameTimeClock.getElapsedTime().asSeconds();
 			//игровое врем€ в секундах идЄт вперед, пока жив игрок. 
@@ -680,7 +641,7 @@ int main()
 			if ((*it)->life) //если пул€ жива, то рисуем еЄ
 				window.draw((*it)->sprite);
 		}
-		if ((p.winStatus == true) || (p.health > 0)) {
+		if ((p.winStatus == true) && (p.health > 0)) {
 			win_text.setString("YOU WON ");//задаем строку тексту
 			win_text.setPosition((1024 / 2 - 210), (768 / 2 - 50));//задаем позицию текста
 			window.draw(win_text);//рисуем этот текст
